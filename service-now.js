d3233ca119573b7e7b159eccda6aa349a3d250fe
Vito Giarrusso <vito.giarrusso@me.com>
2020-09-15 12:37:50 +1000

    add service-now config

---
+++ service-now.js
@@ -0,0 +1,6 @@
+const recommended = require('./index');
+
+const snowRules = { '@babel/no-invalid-this': 'off', 'no-var': 'off' };
+recommended.rules = { ...recommended.rules, ...snowRules };
+
+module.exports = { recommended };
